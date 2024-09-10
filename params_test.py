514b8dcaa551a2de393e61599001e44cd31c4f8b
Ian Samir <falcnpawnch@gmail.com>
2024-09-10 17:03:36 -0400

    improved params test

--- params_test.py
+++ params_test.py
@@ -12,8 +12,8 @@ def run_param_test(param1, param2, range1, range2, optimization_metric="Calmar R
 
     for val1 in range1:
         for val2 in range2:
-            val1 = round(val1, 1)
-            val2 = round(val2, 3)
+            val1 = round(val1, 3)
+            val2 = round(val2, 4)
             
             print(f"testing {param1}: {val1} and {param2}: {val2}...")
             params[param1] = val1
@@ -24,11 +24,16 @@ def run_param_test(param1, param2, range1, range2, optimization_metric="Calmar R
             results.append({param1: val1, param2: val2, optimization_metric: stats[optimization_metric]})
 
     results_df = pd.DataFrame(results)
+    results_df = results_df.drop_duplicates(subset=[param1, param2])
     results_pivot = results_df.pivot(index=param1, columns=param2, values=optimization_metric)
     print(results_pivot)
     plot_heatmap(results_pivot, param1, param2, optimization_metric)
 
 def plot_heatmap(results_df, param1, param2, optimization_metric="Calmar Ratio"):
+    # Sort axes 
+    results_df = results_df.sort_index(ascending=False) 
+    results_df = results_df.reindex(sorted(results_df.columns, key=float), axis=1)
+
     plt.figure(figsize=(10, 6))
     sns.heatmap(results_df, annot=True, fmt=".4f", cmap="YlGnBu", cbar_kws={'label': optimization_metric})
     plt.title(f'{optimization_metric} Heatmap: {param1} vs {param2}')
@@ -37,8 +42,21 @@ def plot_heatmap(results_df, param1, param2, optimization_metric="Calmar Ratio")
     plt.show()
 
 if __name__ == "__main__":
-    param1_range = np.linspace(1, 50, 10) 
-    param2_range = np.linspace(0.001, 0.01, 5) 
-    param1 = "top_n_signals" # top_n_signals, long_sentiment_threshold, short_sentiment_threshold, long_leverage, short_leverage
+    """
+    params to try: 
+
+    top_n_signals
+    long_sentiment_threshold
+    short_sentiment_threshold
+    long_leverage
+    short_leverage
+    return_threshold_window
+    long_return_threshold 
+    short_return_threshold
+
+    """
+    param1_range = np.linspace(1.0, 2.0, 10)  # start, end, increment 
+    param2_range = np.linspace(0.001, 0.0025, 10) 
+    param1 = "long_leverage" 
     param2 = "long_sentiment_threshold"
     run_param_test(param1, param2, param1_range, param2_range)
